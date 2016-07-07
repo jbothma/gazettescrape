c345856a985f6775b95c3dcc04ca4356303d5f45
JD Bothma <jbothma@gmail.com>
2016-07-07 11:45:52 +0200

    Make autothrottle more verbose while getting to know it

--- gazettescrape/settings.py
+++ gazettescrape/settings.py
@@ -82,9 +82,9 @@ AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
